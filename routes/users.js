f0464ef1884cbdefc36d6ee5402bee671db0fda1
moe <mbchehade@gmail.com>
2020-02-09 01:34:50 +0000

    created routes for user, login, logout

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -23,3 +24,68 @@ module.exports = (db) => {
   });
   return router;
 };
+
+
+module.exports = function(router, database){
+//create a new user
+router.post('/', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  database.addUser(user)
+  .then(user => {
+    if(!user){
+      res.send({error: "error"});
+      return;
+    }
+    req.session.userId = user.id;
+    res.send("cookie-made!")
+  })
+  .catch(e -> res.send(e));
+});
+
+
+//check if user exists
+const login = function(email, password) {
+  return database.getUserByEmail(email)
+  .then(user => {
+    if(bcrypt.compareSync(password, user.password)){
+      return user;
+    }
+    return null;
+  })
+}
+exports.login =login;
+
+
+//loging in
+router.post('/', (req, res) => {
+  const {email, password} = req.body;
+  login(emailm password)
+    .then(user => {
+      if(!user){
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      req.send({user: {name: user.name, email: user.email, id: user.id}})
+    })
+    .catch(e => res.send(e));
+});
+
+
+//loging out
+router.post('/', (req, res) => {
+  req.session.userId = null;
+  req.send({});
+})
+
+
+
+
+
+}
+
+
+
+
+
