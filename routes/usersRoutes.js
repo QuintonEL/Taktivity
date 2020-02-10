842f0a9136e852e55eb963b117bc920854f54fd6
moe <mbchehade@gmail.com>
2020-02-10 17:50:14 +0000

    added routes and functions to access our database

---
+++ routes/usersRoutes.js
@@ -0,0 +1,107 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const bcrypt = require('bcrypt');
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
+
+module.exports = function (router, database) {
+
+  // Create a new user
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    database.addUser(user)
+      .then(user => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send("🤗");
+      })
+      .catch(e => res.send(e));
+  });
+
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login = function (email, password) {
+    return database.getUserByEmail(email)
+      .then(user => {
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
+      });
+  }
+  exports.login = login;
+
+  router.post('/', (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({ user: { name: user.name, email: user.email, id: user.id } });
+      })
+      .catch(e => res.send(e));
+  });
+
+  router.post("/logout", (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+  router.get("/myAccount", (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send({ message: "not logged in" });
+      return;
+    }
+
+    database.getUserById(userId)
+      .then(user => {
+        if (!user) {
+          res.send({ error: "no user with that id" });
+          return;
+        }
+
+        res.send({ user: { name: user.name, email: user.email, id: userId } });
+      })
+      .catch(e => res.send(e));
+  });
+
+  return router;
+}
+
+
+
+
+
