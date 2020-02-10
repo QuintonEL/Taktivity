41ef02f984df858f8877c272832f2a113c5b85bb
moe <mbchehade@gmail.com>
2020-02-10 21:31:46 +0000

    added jquery to get from modal signup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,29 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+jQuery(function($){
+  const loggedInUserId = $('#modalBtn').data("userid");
+
+  if (!loggedInUserId) { $('#modalBtn').trigger('click') }
+
+    $("#SignUp").submit(function (event) {
+      event.preventDefault()
+      $.post('/api/users/', $(this).serialize(), function (html) {
+        console.log('all good', html)
+
+       window.location.href = '/'
+
+      }, 'html')
+    });
+
+
+
 });
